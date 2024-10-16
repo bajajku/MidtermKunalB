fe230cfddf789bd40956752b2cdb886b3c8bf3d0
Kunal <bajajku@sheridancollege.ca>
2024-10-16 17:22:55 -0400

    Initial Commit

---
+++ MidtermKunalB/MidtermKunalBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermKunalBApp.swift
+//  MidtermKunalB
+//
+//  Created by Kunal Bajaj on 2024-10-16.
+//
+
+import SwiftUI
+
+@main
+struct MidtermKunalBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MidtermKunalBTests/MidtermKunalBTests.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermKunalBTests.swift
+//  MidtermKunalBTests
+//
+//  Created by Kunal Bajaj on 2024-10-16.
+//
+
+import Testing
+@testable import MidtermKunalB
+
+struct MidtermKunalBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
